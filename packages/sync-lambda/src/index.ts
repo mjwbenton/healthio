065e3a009f9853e04e03930746daddca23261e90
Matthew Benton <m@mattb.tech>
2023-01-05 17:51:05 +0000

    Log event

--- packages/sync-lambda/src/index.ts
+++ packages/sync-lambda/src/index.ts
@@ -1,6 +1,7 @@
 import { APIGatewayEvent } from "aws-lambda";
 
 export async function handler(event: APIGatewayEvent) {
+  console.log(JSON.stringify(event, null, 2));
   return {
     statusCode: 200,
     body: JSON.stringify(event, null, 2),
