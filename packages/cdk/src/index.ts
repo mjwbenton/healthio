ec8dfa6b0f37993b58785c42ab5493e227359a93
Matthew Benton <m@mattb.tech>
2023-01-07 17:12:56 +0000

    Rename stacks

--- packages/cdk/src/index.ts
+++ packages/cdk/src/index.ts
@@ -3,7 +3,7 @@ import HealthioDataStack from "./HealthioDataStack";
 import HealthioSyncStack from "./HealthioSyncStack";
 
 const app = new App();
-const dataStack = new HealthioDataStack(app, "HealthioDataStack");
-new HealthioSyncStack(app, "HealthioSyncStack", {
+const dataStack = new HealthioDataStack(app, "HealthioData");
+new HealthioSyncStack(app, "HealthioSync", {
   dataBucket: dataStack.sourceDataBucket,
 });
