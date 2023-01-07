542acf95550e03a7034856f92a9066d08c445eb4
Matthew Benton <m@mattb.tech>
2023-01-07 19:52:25 +0000

    Sync stack working with custom domain

--- packages/cdk/src/HealthioSyncStack.ts
+++ packages/cdk/src/HealthioSyncStack.ts
@@ -1,9 +1,26 @@
-import { CfnOutput, Stack } from "aws-cdk-lib";
+import { Duration, Fn, Stack } from "aws-cdk-lib";
 import { Construct } from "constructs";
 import { IBucket } from "aws-cdk-lib/aws-s3";
 import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
+import { ARecord, HostedZone, RecordTarget } from "aws-cdk-lib/aws-route53";
+import { DnsValidatedCertificate } from "aws-cdk-lib/aws-certificatemanager";
 import * as path from "path";
 import { FunctionUrlAuthType, Runtime } from "aws-cdk-lib/aws-lambda";
+import {
+  AllowedMethods,
+  CachePolicy,
+  Distribution,
+  OriginProtocolPolicy,
+  OriginRequestPolicy,
+  OriginSslPolicy,
+  ViewerProtocolPolicy,
+} from "aws-cdk-lib/aws-cloudfront";
+import { HttpOrigin } from "aws-cdk-lib/aws-cloudfront-origins";
+import { CloudFrontTarget } from "aws-cdk-lib/aws-route53-targets";
+
+const HOSTED_ZONE = "mattb.tech";
+const HOSTED_ZONE_ID = "Z2GPSB1CDK86DH";
+const DOMAIN_NAME = "sync.healthio.mattb.tech";
 
 export default class HealthioSyncStack extends Stack {
   constructor(
@@ -29,11 +46,37 @@ export default class HealthioSyncStack extends Stack {
       },
     });
     const url = lambda.addFunctionUrl({ authType: FunctionUrlAuthType.NONE });
-
     dataBucket.grantWrite(lambda);
 
-    new CfnOutput(this, "LambdaUrl", {
-      value: url.url,
+    const hostedZone = HostedZone.fromHostedZoneAttributes(this, "HostedZone", {
+      hostedZoneId: HOSTED_ZONE_ID,
+      zoneName: HOSTED_ZONE,
+    });
+
+    const certificate = new DnsValidatedCertificate(this, "Certificate", {
+      domainName: DOMAIN_NAME,
+      hostedZone,
+    });
+
+    const distribution = new Distribution(this, "Distribution", {
+      defaultBehavior: {
+        origin: new HttpOrigin(Fn.select(2, Fn.split("/", url.url)), {
+          protocolPolicy: OriginProtocolPolicy.HTTPS_ONLY,
+          httpsPort: 443,
+        }),
+        viewerProtocolPolicy: ViewerProtocolPolicy.HTTPS_ONLY,
+        allowedMethods: AllowedMethods.ALLOW_ALL,
+        cachePolicy: CachePolicy.CACHING_OPTIMIZED,
+      },
+      certificate,
+      domainNames: [DOMAIN_NAME],
+    });
+
+    new ARecord(this, "DomainRecord", {
+      zone: hostedZone,
+      recordName: DOMAIN_NAME,
+      ttl: Duration.minutes(5),
+      target: RecordTarget.fromAlias(new CloudFrontTarget(distribution)),
     });
   }
 }
