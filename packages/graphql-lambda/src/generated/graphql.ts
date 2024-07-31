70a3e0b3cdf0d11a5adeba84bc2bfe501e535672
Matthew Benton <m@mattb.tech>
2024-07-31 13:20:57 +0100

    Prettier on generated file

--- packages/graphql-lambda/src/generated/graphql.ts
+++ packages/graphql-lambda/src/generated/graphql.ts
@@ -1,10 +1,22 @@
-import { GraphQLResolveInfo, GraphQLScalarType, GraphQLScalarTypeConfig } from 'graphql';
+import {
+  GraphQLResolveInfo,
+  GraphQLScalarType,
+  GraphQLScalarTypeConfig,
+} from "graphql";
 export type Maybe<T> = T | null;
 export type InputMaybe<T> = Maybe<T>;
-export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
-export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> };
-export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
-export type RequireFields<T, K extends keyof T> = Omit<T, K> & { [P in K]-?: NonNullable<T[P]> };
+export type Exact<T extends { [key: string]: unknown }> = {
+  [K in keyof T]: T[K];
+};
+export type MakeOptional<T, K extends keyof T> = Omit<T, K> & {
+  [SubKey in K]?: Maybe<T[SubKey]>;
+};
+export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & {
+  [SubKey in K]: Maybe<T[SubKey]>;
+};
+export type RequireFields<T, K extends keyof T> = Omit<T, K> & {
+  [P in K]-?: NonNullable<T[P]>;
+};
 /** All built-in and custom scalars, mapped to their actual values */
 export type Scalars = {
   ID: string;
@@ -21,22 +33,22 @@ export type Activity = {
 };
 
 export type DistanceDay = {
-  date: Scalars['Date'];
-  km: Scalars['Float'];
-  m: Scalars['Int'];
+  date: Scalars["Date"];
+  km: Scalars["Float"];
+  m: Scalars["Int"];
 };
 
 export type DistanceMonth = {
-  km: Scalars['Float'];
-  m: Scalars['Int'];
-  month: Scalars['Int'];
-  year: Scalars['Int'];
+  km: Scalars["Float"];
+  m: Scalars["Int"];
+  month: Scalars["Int"];
+  year: Scalars["Int"];
 };
 
 export type DistanceTotal = {
   days: Array<DistanceDay>;
-  km: Scalars['Float'];
-  m: Scalars['Int'];
+  km: Scalars["Float"];
+  m: Scalars["Int"];
   months: Array<DistanceMonth>;
 };
 
@@ -44,10 +56,9 @@ export type Query = {
   activity: Activity;
 };
 
-
 export type QueryActivityArgs = {
-  endDate: Scalars['Date'];
-  startDate: Scalars['Date'];
+  endDate: Scalars["Date"];
+  startDate: Scalars["Date"];
 };
 
 export type WithIndex<TObject> = TObject & Record<string, any>;
@@ -55,11 +66,12 @@ export type ResolversObject<TObject> = WithIndex<TObject>;
 
 export type ResolverTypeWrapper<T> = Promise<T> | T;
 
-
 export type ResolverWithResolve<TResult, TParent, TContext, TArgs> = {
   resolve: ResolverFn<TResult, TParent, TContext, TArgs>;
 };
-export type Resolver<TResult, TParent = {}, TContext = {}, TArgs = {}> = ResolverFn<TResult, TParent, TContext, TArgs> | ResolverWithResolve<TResult, TParent, TContext, TArgs>;
+export type Resolver<TResult, TParent = {}, TContext = {}, TArgs = {}> =
+  | ResolverFn<TResult, TParent, TContext, TArgs>
+  | ResolverWithResolve<TResult, TParent, TContext, TArgs>;
 
 export type ResolverFn<TResult, TParent, TContext, TArgs> = (
   parent: TParent,
@@ -82,9 +94,25 @@ export type SubscriptionResolveFn<TResult, TParent, TContext, TArgs> = (
   info: GraphQLResolveInfo
 ) => TResult | Promise<TResult>;
 
-export interface SubscriptionSubscriberObject<TResult, TKey extends string, TParent, TContext, TArgs> {
-  subscribe: SubscriptionSubscribeFn<{ [key in TKey]: TResult }, TParent, TContext, TArgs>;
-  resolve?: SubscriptionResolveFn<TResult, { [key in TKey]: TResult }, TContext, TArgs>;
+export interface SubscriptionSubscriberObject<
+  TResult,
+  TKey extends string,
+  TParent,
+  TContext,
+  TArgs
+> {
+  subscribe: SubscriptionSubscribeFn<
+    { [key in TKey]: TResult },
+    TParent,
+    TContext,
+    TArgs
+  >;
+  resolve?: SubscriptionResolveFn<
+    TResult,
+    { [key in TKey]: TResult },
+    TContext,
+    TArgs
+  >;
 }
 
 export interface SubscriptionResolverObject<TResult, TParent, TContext, TArgs> {
@@ -92,12 +120,26 @@ export interface SubscriptionResolverObject<TResult, TParent, TContext, TArgs> {
   resolve: SubscriptionResolveFn<TResult, any, TContext, TArgs>;
 }
 
-export type SubscriptionObject<TResult, TKey extends string, TParent, TContext, TArgs> =
+export type SubscriptionObject<
+  TResult,
+  TKey extends string,
+  TParent,
+  TContext,
+  TArgs
+> =
   | SubscriptionSubscriberObject<TResult, TKey, TParent, TContext, TArgs>
   | SubscriptionResolverObject<TResult, TParent, TContext, TArgs>;
 
-export type SubscriptionResolver<TResult, TKey extends string, TParent = {}, TContext = {}, TArgs = {}> =
-  | ((...args: any[]) => SubscriptionObject<TResult, TKey, TParent, TContext, TArgs>)
+export type SubscriptionResolver<
+  TResult,
+  TKey extends string,
+  TParent = {},
+  TContext = {},
+  TArgs = {}
+> =
+  | ((
+      ...args: any[]
+    ) => SubscriptionObject<TResult, TKey, TParent, TContext, TArgs>)
   | SubscriptionObject<TResult, TKey, TParent, TContext, TArgs>;
 
 export type TypeResolveFn<TTypes, TParent = {}, TContext = {}> = (
@@ -106,11 +148,20 @@ export type TypeResolveFn<TTypes, TParent = {}, TContext = {}> = (
   info: GraphQLResolveInfo
 ) => Maybe<TTypes> | Promise<Maybe<TTypes>>;
 
-export type IsTypeOfResolverFn<T = {}, TContext = {}> = (obj: T, context: TContext, info: GraphQLResolveInfo) => boolean | Promise<boolean>;
+export type IsTypeOfResolverFn<T = {}, TContext = {}> = (
+  obj: T,
+  context: TContext,
+  info: GraphQLResolveInfo
+) => boolean | Promise<boolean>;
 
 export type NextResolverFn<T> = () => Promise<T>;
 
-export type DirectiveResolverFn<TResult = {}, TParent = {}, TContext = {}, TArgs = {}> = (
+export type DirectiveResolverFn<
+  TResult = {},
+  TParent = {},
+  TContext = {},
+  TArgs = {}
+> = (
   next: NextResolverFn<TResult>,
   parent: TParent,
   args: TArgs,
@@ -121,66 +172,103 @@ export type DirectiveResolverFn<TResult = {}, TParent = {}, TContext = {}, TArgs
 /** Mapping between all available schema types and the resolvers types */
 export type ResolversTypes = ResolversObject<{
   Activity: ResolverTypeWrapper<Activity>;
-  Boolean: ResolverTypeWrapper<Scalars['Boolean']>;
-  Date: ResolverTypeWrapper<Scalars['Date']>;
+  Boolean: ResolverTypeWrapper<Scalars["Boolean"]>;
+  Date: ResolverTypeWrapper<Scalars["Date"]>;
   DistanceDay: ResolverTypeWrapper<DistanceDay>;
   DistanceMonth: ResolverTypeWrapper<DistanceMonth>;
   DistanceTotal: ResolverTypeWrapper<DistanceTotal>;
-  Float: ResolverTypeWrapper<Scalars['Float']>;
-  Int: ResolverTypeWrapper<Scalars['Int']>;
+  Float: ResolverTypeWrapper<Scalars["Float"]>;
+  Int: ResolverTypeWrapper<Scalars["Int"]>;
   Query: ResolverTypeWrapper<{}>;
-  String: ResolverTypeWrapper<Scalars['String']>;
+  String: ResolverTypeWrapper<Scalars["String"]>;
 }>;
 
 /** Mapping between all available schema types and the resolvers parents */
 export type ResolversParentTypes = ResolversObject<{
   Activity: Activity;
-  Boolean: Scalars['Boolean'];
-  Date: Scalars['Date'];
+  Boolean: Scalars["Boolean"];
+  Date: Scalars["Date"];
   DistanceDay: DistanceDay;
   DistanceMonth: DistanceMonth;
   DistanceTotal: DistanceTotal;
-  Float: Scalars['Float'];
-  Int: Scalars['Int'];
+  Float: Scalars["Float"];
+  Int: Scalars["Int"];
   Query: {};
-  String: Scalars['String'];
+  String: Scalars["String"];
 }>;
 
-export type ActivityResolvers<ContextType = any, ParentType extends ResolversParentTypes['Activity'] = ResolversParentTypes['Activity']> = ResolversObject<{
-  swimmingDistance?: Resolver<ResolversTypes['DistanceTotal'], ParentType, ContextType>;
-  walkingRunningDistance?: Resolver<ResolversTypes['DistanceTotal'], ParentType, ContextType>;
+export type ActivityResolvers<
+  ContextType = any,
+  ParentType extends ResolversParentTypes["Activity"] = ResolversParentTypes["Activity"]
+> = ResolversObject<{
+  swimmingDistance?: Resolver<
+    ResolversTypes["DistanceTotal"],
+    ParentType,
+    ContextType
+  >;
+  walkingRunningDistance?: Resolver<
+    ResolversTypes["DistanceTotal"],
+    ParentType,
+    ContextType
+  >;
   __isTypeOf?: IsTypeOfResolverFn<ParentType, ContextType>;
 }>;
 
-export interface DateScalarConfig extends GraphQLScalarTypeConfig<ResolversTypes['Date'], any> {
-  name: 'Date';
+export interface DateScalarConfig
+  extends GraphQLScalarTypeConfig<ResolversTypes["Date"], any> {
+  name: "Date";
 }
 
-export type DistanceDayResolvers<ContextType = any, ParentType extends ResolversParentTypes['DistanceDay'] = ResolversParentTypes['DistanceDay']> = ResolversObject<{
-  date?: Resolver<ResolversTypes['Date'], ParentType, ContextType>;
-  km?: Resolver<ResolversTypes['Float'], ParentType, ContextType>;
-  m?: Resolver<ResolversTypes['Int'], ParentType, ContextType>;
+export type DistanceDayResolvers<
+  ContextType = any,
+  ParentType extends ResolversParentTypes["DistanceDay"] = ResolversParentTypes["DistanceDay"]
+> = ResolversObject<{
+  date?: Resolver<ResolversTypes["Date"], ParentType, ContextType>;
+  km?: Resolver<ResolversTypes["Float"], ParentType, ContextType>;
+  m?: Resolver<ResolversTypes["Int"], ParentType, ContextType>;
   __isTypeOf?: IsTypeOfResolverFn<ParentType, ContextType>;
 }>;
 
-export type DistanceMonthResolvers<ContextType = any, ParentType extends ResolversParentTypes['DistanceMonth'] = ResolversParentTypes['DistanceMonth']> = ResolversObject<{
-  km?: Resolver<ResolversTypes['Float'], ParentType, ContextType>;
-  m?: Resolver<ResolversTypes['Int'], ParentType, ContextType>;
-  month?: Resolver<ResolversTypes['Int'], ParentType, ContextType>;
-  year?: Resolver<ResolversTypes['Int'], ParentType, ContextType>;
+export type DistanceMonthResolvers<
+  ContextType = any,
+  ParentType extends ResolversParentTypes["DistanceMonth"] = ResolversParentTypes["DistanceMonth"]
+> = ResolversObject<{
+  km?: Resolver<ResolversTypes["Float"], ParentType, ContextType>;
+  m?: Resolver<ResolversTypes["Int"], ParentType, ContextType>;
+  month?: Resolver<ResolversTypes["Int"], ParentType, ContextType>;
+  year?: Resolver<ResolversTypes["Int"], ParentType, ContextType>;
   __isTypeOf?: IsTypeOfResolverFn<ParentType, ContextType>;
 }>;
 
-export type DistanceTotalResolvers<ContextType = any, ParentType extends ResolversParentTypes['DistanceTotal'] = ResolversParentTypes['DistanceTotal']> = ResolversObject<{
-  days?: Resolver<Array<ResolversTypes['DistanceDay']>, ParentType, ContextType>;
-  km?: Resolver<ResolversTypes['Float'], ParentType, ContextType>;
-  m?: Resolver<ResolversTypes['Int'], ParentType, ContextType>;
-  months?: Resolver<Array<ResolversTypes['DistanceMonth']>, ParentType, ContextType>;
+export type DistanceTotalResolvers<
+  ContextType = any,
+  ParentType extends ResolversParentTypes["DistanceTotal"] = ResolversParentTypes["DistanceTotal"]
+> = ResolversObject<{
+  days?: Resolver<
+    Array<ResolversTypes["DistanceDay"]>,
+    ParentType,
+    ContextType
+  >;
+  km?: Resolver<ResolversTypes["Float"], ParentType, ContextType>;
+  m?: Resolver<ResolversTypes["Int"], ParentType, ContextType>;
+  months?: Resolver<
+    Array<ResolversTypes["DistanceMonth"]>,
+    ParentType,
+    ContextType
+  >;
   __isTypeOf?: IsTypeOfResolverFn<ParentType, ContextType>;
 }>;
 
-export type QueryResolvers<ContextType = any, ParentType extends ResolversParentTypes['Query'] = ResolversParentTypes['Query']> = ResolversObject<{
-  activity?: Resolver<ResolversTypes['Activity'], ParentType, ContextType, RequireFields<QueryActivityArgs, 'endDate' | 'startDate'>>;
+export type QueryResolvers<
+  ContextType = any,
+  ParentType extends ResolversParentTypes["Query"] = ResolversParentTypes["Query"]
+> = ResolversObject<{
+  activity?: Resolver<
+    ResolversTypes["Activity"],
+    ParentType,
+    ContextType,
+    RequireFields<QueryActivityArgs, "endDate" | "startDate">
+  >;
 }>;
 
 export type Resolvers<ContextType = any> = ResolversObject<{
@@ -191,4 +279,3 @@ export type Resolvers<ContextType = any> = ResolversObject<{
   DistanceTotal?: DistanceTotalResolvers<ContextType>;
   Query?: QueryResolvers<ContextType>;
 }>;
-
